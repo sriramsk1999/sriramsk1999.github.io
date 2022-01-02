137a3fcea616f89043812f702e36e7842b8afde4
Sriram S K <sriramsk1999@gmail.com>
2022-01-02 16:38:21 +0530

    update quotes

--- quotes.js
+++ quotes.js
@@ -1,14 +1,15 @@
 // Return random quote
 
 function formatQuote(quote, author) {
-    quote = quote.bold() + " - " + author;
+    quote = quote.bold() + "<br> - " + author;
     quote = "<blockquote>" + quote + "</blockquote>";
     return quote;
 }
 
 quotes = [
-    [`A man's reach should exceed his grasp, or what's a heaven for?`, `Robert Browning`],
-    [`The most important step a man can take. It's not the first one, is it? It's the next one. Always the next step, Dalinar.`, `Dalinar Kholin`],
+    ["Ah, but a man's reach should exceed his grasp, <br>Or what's a heaven for?", "Robert Browning"],
+    ["The most important step a man can take. It's not the first one, is it? It's the next one. Always the next step, Dalinar.", "Dalinar Kholin"],
+    ["It matters not how strait the gate, <br>How charged with punishments the scroll, <br>I am the master of my fate,<br>I am the captain of my soul.\n", "William Ernest Henley"]
 ]
 
 quote = quotes[Math.floor(Math.random() * quotes.length)];
